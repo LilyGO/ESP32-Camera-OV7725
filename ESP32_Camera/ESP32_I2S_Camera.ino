99f4981a111d2808a28c84a692273d5639eedc10
Lily <lily@lilygo.cc>
2017-12-13 09:45:29 +0800

    first commit

---
+++ ESP32_Camera/ESP32_I2S_Camera.ino
@@ -0,0 +1,158 @@
+#include "OV7670.h"
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClient.h>
+#include "BMP.h"
+
+const int SIOD = 21; //SDA
+const int SIOC = 22; //SCL
+
+const int VSYNC = 34;
+const int HREF = 35;
+
+const int XCLK = 32;
+const int PCLK = 33;
+
+const int D0 = 27;
+const int D1 = 17;
+const int D2 = 16;
+const int D3 = 15;
+const int D4 = 14;
+const int D5 = 13;
+const int D6 = 12;
+const int D7 = 4;
+
+const int TFT_DC = 2;
+const int TFT_CS = 5;
+//DIN <- MOSI 23
+//CLK <- SCK 18
+
+#define ssid1        "TP-LINK_888"  //Your wiff id
+#define password1    "Zhang520521" //Your wiff password
+//#define ssid2        ""
+//#define password2    ""
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, 0/*no reset*/);
+OV7670 *camera;
+
+WiFiMulti wifiMulti;
+WiFiServer server(80);
+
+unsigned char bmpHeader[BMP::headerSize];
+
+void serve()
+{
+  WiFiClient client = server.available();
+  if (client) 
+  {
+    //Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        //Serial.write(c);
+        if (c == '\n') 
+        {
+          if (currentLine.length() == 0) 
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print(
+              "<style>body{margin: 0}\nimg{height: 100%; width: auto}</style>"
+              "<img id='a' src='/camera' onload='this.style.display=\"initial\"; var b = document.getElementById(\"b\"); b.style.display=\"none\"; b.src=\"camera?\"+Date.now(); '>"
+              "<img id='b' style='display: none' src='/camera' onload='this.style.display=\"initial\"; var a = document.getElementById(\"a\"); a.style.display=\"none\"; a.src=\"camera?\"+Date.now(); '>");
+            client.println();
+            break;
+          } 
+          else 
+          {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') 
+        {
+          currentLine += c;
+        }
+        
+        if(currentLine.endsWith("GET /camera"))
+        {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:image/bmp");
+            client.println();
+            
+            for(int i = 0; i < BMP::headerSize; i++)
+               client.write(bmpHeader[i]);
+            for(int i = 0; i < camera->xres * camera->yres * 2; i++)
+               client.write(camera->frame[i]);
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    //Serial.println("Client Disconnected.");
+  }  
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  wifiMulti.addAP(ssid1, password1);
+  //wifiMulti.addAP(ssid2, password2);
+  Serial.println("Connecting Wifi...");
+  if(wifiMulti.run() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+  }
+  
+  camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
+  BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
+  
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(0);
+  server.begin();
+}
+
+void displayY8(unsigned char * frame, int xres, int yres)
+{
+  tft.setAddrWindow(0, 0, yres - 1, xres - 1);
+  int i = 0;
+  for(int x = 0; x < xres; x++)
+    for(int y = 0; y < yres; y++)
+    {
+      i = y * xres + x;
+      unsigned char c = frame[i];
+      unsigned short r = c >> 3;
+      unsigned short g = c >> 2;
+      unsigned short b = c >> 3;
+      tft.pushColor(r << 11 | g << 5 | b);
+    }  
+}
+
+void displayRGB565(unsigned char * frame, int xres, int yres)
+{
+  tft.setAddrWindow(0, 0, yres - 1, xres - 1);
+  int i = 0;
+  for(int x = 0; x < xres; x++)
+    for(int y = 0; y < yres; y++)
+    {
+      i = (y * xres + x) << 1;
+      tft.pushColor((frame[i] | (frame[i+1] << 8)));
+    }  
+}
+
+void loop()
+{
+  camera->oneFrame();
+  serve();
+  displayRGB565(camera->frame, camera->xres, camera->yres);
+}
---
+++ ESP32_Camera/README.md
@@ -0,0 +1,17 @@
+# ESP32 I2S Camera
+
+This project shows how to record images with the ESP32 and the OV7670 camera without FIFO. An SPI TFT display is supported and a basic web server provides the images in the local network.
+
+The video explaining this code can be found here
+https://youtu.be/S2yTQHM82jc
+
+Also check out the project page for some details, parts and datasheets
+http://bitluni.net/esp32-i2s-camera-ov7670/
+
+# Acknowledgements
+The project is based on the esp-cam-demo by Ivan Grokhotkov
+https://github.com/igrr/esp32-cam-demo
+
+#License
+Since parts of the code are taken from the esp-cam-demo which are subject to the Apache License 2.0 license. You will find a note in at the top of the respective files.
+The rest of the code is Public Domain
